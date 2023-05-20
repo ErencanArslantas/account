48e3ff1570976d449845d7aa70dc77436bcc343f
fatmanur yılmaz <120799395+fatmanur-yilmaz@users.noreply.github.com>
2023-05-20 17:00:37 +0300

    initial commit

---
+++ src/main/java/com/folksdev/account/controller/AccountController.java
@@ -0,0 +1,27 @@
+package com.folksdev.account.controller;
+
+
+import com.folksdev.account.dto.AccountDto;
+import com.folksdev.account.dto.CreateAccountRequest;
+import com.folksdev.account.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/account")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+// validation geçerli olması için controller kısmına eklemeyi unutma
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@Valid @RequestBody CreateAccountRequest request){
+        return ResponseEntity.ok(accountService.createAccount(request));
+    }
+
+}
---
+++ src/test/java/com/folksdev/account/service/CustomerServiceTest.java
@@ -0,0 +1,82 @@
+package com.folksdev.account.service;
+
+import com.folksdev.account.dto.CustomerAccountDto;
+import com.folksdev.account.dto.CustomerDto;
+import com.folksdev.account.dto.CustomerDtoConverter;
+import com.folksdev.account.exception.CustomerNotFoundException;
+import com.folksdev.account.model.Customer;
+import com.folksdev.account.repository.CustomerRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+
+public class CustomerServiceTest {
+
+  private CustomerService service;
+  private CustomerRepository customerRepository;
+  private CustomerDtoConverter converter;
+// private static metotların testleri yazılmaz
+  @BeforeEach
+  public void setUp(){
+   customerRepository = mock(CustomerRepository.class);
+   converter = mock(CustomerDtoConverter.class);
+   service = new CustomerService(customerRepository,converter);
+
+  }
+    @Test
+    public void testFindByCustomerId_whenCustomerIdExists_shouldReturnCustomer(){
+        Customer customer = new Customer("id","name","surname",Set.of());
+
+        Mockito.when(customerRepository.findById("id")).thenReturn(Optional.of(customer));
+
+        Customer result = service.findCustomerById("id");
+
+        assertEquals(result,
+                customer);
+    }
+  @Test
+ public void testFindByCustomerId_whenCustomerIdDoesNotExist_shouldThrowCustomerNotFoundException(){
+
+
+
+      Mockito.when(customerRepository.findById("id")).thenReturn(Optional.empty());
+
+      assertThrows(CustomerNotFoundException.class, () -> service.findCustomerById("id"));
+
+
+  }
+  @Test
+  public void testGetCustomerById_whenCustomerIdExists_shouldReturnCustomer(){
+      Customer customer = new Customer("id","name","surname",Set.of());
+      CustomerDto customerDto = new CustomerDto("id","name ","surname",Set.of());
+
+      Mockito.when(customerRepository.findById("id")).thenReturn(Optional.of(customer));
+
+      Mockito.when(converter.convertToCustomerDto(customer)).thenReturn(customerDto);
+
+      CustomerDto  result = service.getCustomerById("id");
+
+      assertEquals(result,customer);
+  }
+    @Test
+    public void testGetCustomerById_whenCustomerIdDoesNotExist_shouldThrowCustomerNotFoundException() {
+
+
+        Mockito.when(customerRepository.findById("id")).thenReturn(Optional.empty());
+
+
+        assertThrows(CustomerNotFoundException.class, () -> service.getCustomerById("id"));
+
+        Mockito.verifyNoInteractions(converter); //converterın hiçbir methodunu çağırma
+
+    }
+
+}
